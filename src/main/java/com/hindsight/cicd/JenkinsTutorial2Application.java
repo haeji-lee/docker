bd64a7d3ca9c6b4e03f7c4bf4e7edca809925a62
haeji-lee <yellowhyeji@gmail.com>
2022-04-06 20:39:14 +0900

    first commit

---
+++ src/main/java/com/hindsight/cicd/JenkinsTutorial2Application.java
@@ -0,0 +1,13 @@
+package com.hindsight.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTutorial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTutorial2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hindsight/cicd/JenkinsTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hindsight.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTutorial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
